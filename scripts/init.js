4d61644bd678fd9f2d01948a634e9149b61ad877
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-05-02 21:32:18 +0800

    fix shit

--- scripts/init.js
+++ scripts/init.js
@@ -1,34 +1,17 @@
 const ui = require("ui-lib/library");
-const mcp = extend(SwitchBlock, "missile-control-panel", {
-	"name": "Missile Control Panel",
-	"description": "Launch Missiles at your enemies with this 2x2 control panel.",
-
-	"size": 2,
-	"squareSprite": false,
-	
-	"configurable": true,
-
-	"consumes": {
-		"power": 2,
-		"items": {
-			"items": [
-				"thorium/3",
-				"spore-pod/2"
-			]
-		}
-	},
-
-	"destructible": true,
-	"solid": true,
-
-	"requirements": [
-		{"item": "silicon", "amount": 460},
-		{"item": "lead", "amount": 160},
-		{"item": "thorium", "amount": 140}
-	],
-	
-	"category": "crafting",
-	"research": "item-uranium"
-})
+const mcp = extend(GenericCrafter, "missile-control-panel", {
+	"power": 2,
+	"items": {
+		"items": [
+			"thorium/3",
+			"spore-pod/2"
+		]
+	}
+});
+mcp.destructible = true;
+mcp.solid = true;
+mcp.requirements = ItemStack.with(Items.silicon, 460, Items.lead, 160, Items.thorium, 140);
+mcp.category = Category.crafting;
+mcp.research = "item-uranium";
 
 print("Atomdustry initalized");
